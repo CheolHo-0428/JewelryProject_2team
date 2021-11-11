1a8a805638bb379699fc745c090b504f9bc66c07
Lee-YongJun <0311odi@gmail.com>
2021-11-11 09:30:52 +0900

    20211111

--- front/vue-frontend/src/components/Header.vue
+++ front/vue-frontend/src/components/Header.vue
@@ -72,9 +72,9 @@
         </div>
       </div>
     </nav>
+    <!-- <button @click="requestPay">결제하기</button> -->
   </div>
 </template>
-
 <script>
 export default {
   data () {
@@ -94,6 +94,29 @@ export default {
     }
   },
   methods: {
+    // requestPay: function () {
+    //   var IMP = window.IMP // 생략 가능
+    //   IMP.init('{imp73794280}') // 예: imp00000000
+    //   // IMP.request_pay(param, callback) 결제창 호출
+    //   IMP.request_pay({ // param
+    //     pg: 'html5_inicis',
+    //     pay_method: 'card',
+    //     merchant_uid: 'ORD20180131-0000011',
+    //     name: '노르웨이 회전 의자',
+    //     amount: 64900,
+    //     buyer_email: 'gildong@gmail.com',
+    //     buyer_name: '홍길동',
+    //     buyer_tel: '010-4242-4242',
+    //     buyer_addr: '서울특별시 강남구 신사동',
+    //     buyer_postcode: '01181'
+    //   }, rsp => { // callback
+    //     if (rsp.success) {
+    //       // 결제 성공 시 로직,
+    //     } else {
+    //       // 결제 실패 시 로직,
+    //     }
+    //   })
+    // },
     logOut () {
       this.$store.dispatch('auth/logout')
       this.$swal.fire({
